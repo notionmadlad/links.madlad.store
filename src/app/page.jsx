667d6922dd96bbd440af3b68bb6966d2eae3ed71
sudharshans2009 <sudharshans2009@gmail.com>
2024-05-30 21:25:38 +0530

    Added Background Beams and Sparkles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { footerLinks, mainLinks, socialLinks } from "@/config/main";
 import Link from "next/link";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function Home() {
   return (
@@ -17,7 +19,7 @@ export default function Home() {
               <Image src="/madlad-pfp.png" className="rounded-full" width={96} height={96} alt="madlad-pfp" />
               <h1 className="text-[18px] font-semibold">Coding Madlad</h1>
               <p className="text-muted-foreground mx-10 text-center">Get your life and business on track with my tips and Notion Templates.</p>
-              <div className="flex flex-col gap-5 w-full">
+              <div className="flex flex-col gap-5 w-full z-[1]">
                 {Object.keys(mainLinks).map(link => (
                   <Link href={mainLinks[link].link} key={link}>
                     <div className="flex bg-card hover:bg-secondary transition-all duration-300 w-full border border-border rounded-lg overflow-hidden cursor-pointer">
@@ -30,7 +32,7 @@ export default function Home() {
                   </Link>
                 ))}
               </div>
-              <div className="flex justify-between gap-10">
+              <div className="flex justify-between gap-10 z-[1]">
                 {Object.keys(socialLinks).map((link) => {
                   const Icon = socialLinks[link].icon;
                   return (
@@ -40,11 +42,23 @@ export default function Home() {
                   )
                 })}
               </div>
+              <div className="absolute h-screen top-0 left-0 right-0">
+                <SparklesCore
+                  id="tsparticlesfullpage"
+                  background="transparent"
+                  minSize={0.3}
+                  maxSize={1}
+                  particleDensity={60}
+                  className="w-full h-full"
+                  particleColor="#777777"
+                />
+              </div>
+              <BackgroundBeams />
             </div>
           </div>
         </div>
       </section>
-      <div className="fixed top-5 left-5 flex justify-between gap-5">
+      <div className="fixed top-5 left-5 flex justify-between gap-5 z-[9999]">
         {Object.keys(footerLinks).map((link) => (
           <Link className="text-muted-foreground text-[14px] p-2 bg-card rounded-md leading-6" href={footerLinks[link]} key={link}>
             {link}
